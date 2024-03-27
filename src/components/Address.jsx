8a772880780581dab93ddad6a14291f970f29255
Sanjana M <sanjanamurugesan06@gmail.com>
2024-03-28 00:17:26 +0530

    .

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function Address(){
     return(
-        <div>
-            Address
+        <div className="addr-main">
+            <div>
+                <i>
+                    Home
+                </i>
+            </div>
+            <div>
+                <h5>Home</h5>
+                <p>1E, Ganganagar, Muthu Nagar, Narasimhanaickenpalayam, Tamil Nadu 641031, India</p>
+                <h6>29 MINS</h6>
+                <button>DELIVER HERE</button>
+            </div>
         </div>
     );
 }
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -4,19 +4,26 @@ import Cart from "./Cart";
 import Address from "./Address";
 function Checkout() {
   return (
-    <div>
+    <div className="c-main">
       <Header></Header>
       <div className="checkout-main">
-        <div className="checkout-container">
-          <div> 
+        <div>
+          <div className="checkout-container">
+            <h5 className="margin-zero">Select delivery Address</h5>
+            <p className="margin-zero">
+              You have a saved address in this location
+            </p>
             <div className="checkout-address">
               <Address></Address>
               <Address></Address>
             </div>
-            <div></div>
           </div>
-          <Cart></Cart>
+          <div className="checkout-payment">
+            <p>Payment</p>
+          </div>
         </div>
+
+        <Cart></Cart>
       </div>
     </div>
   );
